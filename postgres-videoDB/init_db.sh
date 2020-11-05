755988ccde41bb156d8d4946e6026f7dbbae999f
klemenStanic <ks4211@student.uni-lj.si>
2020-11-05 16:22:22 +0100

    added Dockerfile

--- postgres-videoDB/init_db.sh
+++ postgres-videoDB/init_db.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+
+name="video-db"
+
+# Only run db if video-db containter isnt already running
+[[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] || 
 sudo docker run -d --name video-db -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=video-db -p 5432:5432 postgres:13
 cat sql-scripts/init.sql | sudo docker exec -i video-db psql -U dbuser -d video-db
