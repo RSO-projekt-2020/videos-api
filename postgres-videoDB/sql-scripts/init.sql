78f344de290b793875bca5a9f5f6d9056d2bc418
Luka Kavčič <luka.kavcic95@gmail.com>
2020-12-03 18:11:06 +0100

    updated sql script

--- postgres-videoDB/sql-scripts/init.sql
+++ postgres-videoDB/sql-scripts/init.sql
@@ -10,4 +10,7 @@ CREATE TABLE IF NOT EXISTS videos
 	created_on TIMESTAMP NOT NULL,
 	path VARCHAR ( 1000 ) UNIQUE NOT NULL
 );
-INSERT INTO videos (video_id, user_id, title, description, width, height, created_on, path) VALUES (1,1,'Video title', 'Video description', 1920, 1080, '2006-01-01 15:36:38', 'path/to/video/video.mp4');
+INSERT INTO videos (video_id, user_id, title, description, width, height, created_on, path) VALUES (1,1,'Video title 1', 'Video description 1', 1920, 1080, '2006-01-01 15:36:38', 'path/to/video/video1.mp4');
+INSERT INTO videos (video_id, user_id, title, description, width, height, created_on, path) VALUES (2,2,'Video title 2', 'Video description 2', 1920, 1080, '2006-01-02 15:36:38', 'path/to/video/video2.mp4');
+INSERT INTO videos (video_id, user_id, title, description, width, height, created_on, path) VALUES (3,1,'Video title 3', 'Video description 3', 1920, 1080, '2006-01-03 15:36:38', 'path/to/video/video3.mp4');
+INSERT INTO videos (video_id, user_id, title, description, width, height, created_on, path) VALUES (4,2,'Video title 4', 'Video description 4', 1920, 1080, '2006-01-04 15:36:38', 'path/to/video/video4.mp4');
